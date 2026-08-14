fba21bec665007cee2ea83d0239b15b99ae29716
diego <sargent.pointed453@passinbox.com>
2026-08-14 12:11:42 +0200

    Add value attribute to CropType and update crop definitions

--- src/main/java/com/sfarm4j/data/CropType.java
+++ src/main/java/com/sfarm4j/data/CropType.java
@@ -1,24 +1,26 @@
 package com.sfarm4j.data;
 
 public enum CropType {
-    WHEAT((byte) 0, "Wheat", 1, 4),
-    CARROT((byte) 1, "Carrot", 4, 3),
-    POTATO((byte) 2, "Potato", 6, 6),
-    CABBAGE((byte) 3, "Cabbage", 8, 4),;
+    WHEAT((byte) 0, "Wheat", 1, 4, 5),
+    CARROT((byte) 1, "Carrot", 4, 3, 8),
+    POTATO((byte) 2, "Potato", 6, 6, 12),
+    CABBAGE((byte) 3, "Cabbage", 8, 4, 16),;
 
     private final byte id;
     private final String name;
     private final int daysToGrow;
     private final int yield;
+    private final int value;
 
     CropType(byte id,
              String name,
              int daysToGrow,
-             int yield) {
+             int yield, int value) {
         this.id = id;
         this.name = name;
         this.daysToGrow = daysToGrow;
         this.yield = yield;
+        this.value = value;
     }
 
     public byte getId() {
@@ -35,6 +37,10 @@ public enum CropType {
 
     public int getYield() {
         return yield;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public static CropType fromId(byte id) {
