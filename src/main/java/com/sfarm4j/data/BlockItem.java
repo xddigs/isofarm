78f9f36786ab43d8c80fb7e9a8f36f7202947e7b
diego <sargent.pointed453@passinbox.com>
2026-08-15 14:54:09 +0200

    Remove dead code

--- src/main/java/com/sfarm4j/data/BlockItem.java
+++
@@ -1,9 +0,0 @@
-package com.sfarm4j.data;
-
-@DataClass
-public class BlockItem extends Item {
-
-    public BlockItem(byte id, String name, int value) {
-        super(id, name, 1, value);
-    }
-}
