e6074413deed298b0ea1663a0a9f294099c13d48
diego <sargent.pointed453@passinbox.com>
2026-08-17 23:31:28 +0200

    Add UIPanel class

---
+++ src/main/java/com/tilled/gui/UIPanel.java
@@ -0,0 +1,15 @@
+package com.tilled.gui;
+
+import com.tilled.data.UIElement;
+
+public class UIPanel extends UIElement {
+
+    public UIPanel(float x, float y, float width, float height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void render(UIRenderer renderer) {
+
+    }
+}
