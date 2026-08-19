78a8f33bff891cfeb97da3340b69b83e70575d49
diego <sargent.pointed453@passinbox.com>
2026-08-19 20:55:09 +0200

    Add tier IDs to Tier enum and update constructor

--- src/main/java/com/tilled/data/Tier.java
+++ src/main/java/com/tilled/data/Tier.java
@@ -1,19 +1,25 @@
 package com.tilled.data;
 
 public enum Tier {
-    COPPER("Copper", 128),
-    IRON("Iron", 160),
-    STEEL("Steel", 192),
-    GOLD("Gold", 64),
-    PLATINUM("Platinum", 224),
-    DIAMOND("Diamond", 1024);
+    COPPER((byte) 1, "Copper", 128),
+    IRON((byte) 2, "Iron", 160),
+    STEEL((byte) 3, "Steel", 192),
+    GOLD((byte) 4, "Gold", 64),
+    PLATINUM((byte) 5, "Platinum", 224),
+    DIAMOND((byte) 6, "Diamond", 1024);
 
+    private final byte id;
     private final String name;
     private final int durability;
 
-    Tier(String name, int durability) {
+    Tier(byte id, String name, int durability) {
+        this.id = id;
         this.name = name;
         this.durability = durability;
+    }
+
+    public byte getId() {
+        return id;
     }
 
     public String getName() {
