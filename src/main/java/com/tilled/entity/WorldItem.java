b6759f7d39be2563cd99b795935fdab79393787d
diego <sargent.pointed453@passinbox.com>
2026-08-20 15:53:51 +0200

    Fix ground offset calculation

--- src/main/java/com/tilled/entity/WorldItem.java
+++ src/main/java/com/tilled/entity/WorldItem.java
@@ -13,7 +13,7 @@ import org.joml.Vector3f;
 public class WorldItem extends Entity {
 
     private static final float GRAVITY = -20.0f;
-    private static final float GROUND_OFFSET = 0.05f;
+    private static final float GROUND_OFFSET = 0.20f;
 
     private static final float PICKUP_DELAY = 0.56f;
 
