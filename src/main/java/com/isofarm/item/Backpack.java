880b11a4b3ac6598f2628cbeab4b216e8d4baba6
diego <sargent.pointed453@passinbox.com>
2026-08-23 17:26:30 +0200

    Add the backpack

---
+++ src/main/java/com/isofarm/item/Backpack.java
@@ -0,0 +1,26 @@
+package com.isofarm.item;
+
+import com.isofarm.data.Tier;
+import com.isofarm.data.ToolType;
+import com.isofarm.wrld.GameMaster;
+
+public class Backpack extends Tool {
+
+    public Backpack(ToolType type, Tier tier) {
+        super((byte) 2, tier.getName() + " Backpack", 100, type, tier,
+                tier.getDurability() + type.getBaseDurability());
+    }
+
+    public Backpack() {
+        this(ToolType.BACKPACK, Tier.LEATHER);
+    }
+
+    @Override
+    public Item copy() {
+        return new Backpack(ToolType.BACKPACK, Tier.LEATHER);
+    }
+
+    public void use(GameMaster gameMaster) {
+        setPlayer(gameMaster.getPlayer());
+    }
+}
