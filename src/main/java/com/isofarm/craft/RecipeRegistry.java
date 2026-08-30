4c81bb56816f7d8558e760d0d1c33af6311af304
diego <sargent.pointed453@passinbox.com>
2026-08-30 20:16:57 +0200

    Remove old recipe for crafting book

--- src/main/java/com/isofarm/craft/RecipeRegistry.java
+++ src/main/java/com/isofarm/craft/RecipeRegistry.java
@@ -18,7 +18,6 @@ public class RecipeRegistry {
         create(Tier.LEATHER).result(new Material(Tier.NONE, MaterialID.STICK), 4).with(BlockData.OAK_WOOD, 1).add();
         create(Tier.LEATHER).result(new Material(Tier.NONE, MaterialID.BOOK), 1).with(MaterialID.LEATHER, 3).with(MaterialID.PAPER, 2).add();
         create(Tier.LEATHER).result(new Bucket(BlockData.AIR, Tier.WOOD), 1).with(BlockData.OAK_WOOD, 3).add();
-        create(Tier.LEATHER).result(new CraftingBook(), 1).with(MaterialID.LEATHER, 2).add();
 
         registerToolSet(Tier.LEATHER, BlockData.OAK_WOOD);
 
