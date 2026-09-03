8f8d870832b9d6158193483b1b20cb90641b8871
diego <sargent.pointed453@passinbox.com>
2026-09-03 17:43:16 +0200

    Add english keys for localization and base Local class

---
+++ src/main/java/com/isofarm/utils/Local.java
@@ -0,0 +1,41 @@
+package com.isofarm.utils;
+
+import com.isofarm.data.Singleton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.Properties;
+
+@Utils
+@Singleton
+public class Local {
+    public static final Local lang = new Local();
+    private final Properties properties = new Properties();
+
+    private Local() {
+        load(Locale.getDefault().getLanguage());
+    }
+
+    private void load(String language) {
+        String file = "lang/lang-" + language + ".properties";
+        try (InputStream input = Local.class.getResourceAsStream(file)) {
+            if (input == null) {
+                load("en");
+                return;
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load localization: " + file, e);
+        }
+    }
+
+    public String t(String key) {
+        return properties.getProperty(key, key);
+    }
+
+    public String f(String s, Object... args) {
+        return MessageFormat.format(Local.lang.t(s), args);
+    }
+}
